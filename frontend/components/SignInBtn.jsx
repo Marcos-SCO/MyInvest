9c483cd44db6baa40f79f37fb5ceff396bc728ea
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2023-08-16 01:02:03 -0300

    refactor: displaying svg itens in plain text

--- frontend/components/SignInBtn.jsx
+++ frontend/components/SignInBtn.jsx
@@ -7,8 +7,7 @@ import DisplaySvg from "@/app/helpers/DisplaySvg";
 export default function SignInBtn() {
   return (
     <button onClick={() => signIn('google')} className="flex items-center gap-4 shadow-xl rounded-lg pl-3">
-      {/* <Image src={'/google-logo.svg'} height={30} width={30} /> */}
-      <DisplaySvg name="googleIcon" height={30} width={30} />
+      <DisplaySvg name="googleIcon"/>
       <span className="bg-blue-500 text-white px-4 py-3">Login com Google</span>
     </button>
   );
