85d7f088b91c5283ba92762e083ae56b90b1c980
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2023-11-21 23:42:48 -0300

    fix: change sing and sinup page names

--- frontend/app/signIn/page.jsx
+++ frontend/app/signIn/page.jsx
@@ -1,9 +1,13 @@
 import SignInForm from "./form/SignInForm";
 
+import ChangePageAttributes from "../../app/hooks/ChangePageAttributes";
+
 export const metadata = {}
 
 export default function signIn() {
   metadata.title = `MyInvest - Login`;
+
+  <ChangePageAttributes pageName={'signIn'} />
 
   return (
     <div className='signIn'>
--- frontend/app/signUp/page.jsx
+++ frontend/app/signUp/page.jsx
@@ -1,9 +1,13 @@
 import SignUpForm from "./form/SignUpForm";
 
+import ChangePageAttributes from "../../app/hooks/ChangePageAttributes";
+
 export const metadata = {}
 
 export default function signIn() {
   metadata.title = `MyInvest - Cadastrar`;
+
+  <ChangePageAttributes pageName={'signUp'} />
 
   return (
     <div className='signUp'>
