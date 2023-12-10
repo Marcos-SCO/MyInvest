24715e47aed43df17a678b9ccc1c010674e02e74
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2023-12-10 15:04:41 -0300

    build: module path

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -19,7 +19,7 @@ import { UrlChangeListener } from './hooks/UrlChangeListener';
 import ChangePageAttributes from "app/hooks/ChangePageAttributes";
 import ProgressBar from 'components/page/ProgressBar';
 
-import SearchBarContainer from "components/searchBar/SearchBarContainer";
+import SearchBarContainer from "../components/searchBar/SearchBarContainer";
 
 import { nextAuthOptions } from './api/auth/[...nextauth]/route';
 import { getServerSession } from 'next-auth';
